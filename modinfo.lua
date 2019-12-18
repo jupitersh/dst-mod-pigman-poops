176e9ae36eccbca99d3113377e4758bf329691e6
jupitersh <arxhd.love@qq.com>
2019-12-19 00:30:16 +0800

    first commit

---
+++ modinfo.lua
@@ -0,0 +1,19 @@
+name = "Pigman Poops"
+
+description = "This mod makes pigman poop just like beefalo."
+
+author = "[BSy]Jupiter"
+
+version = " 1.0"
+
+api_version = 10
+
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+dst_compatible = true
+all_clients_require_mod = true
+
+forumthread = ""
+
+icon = "modicon.tex"
+icon_atlas = "modicon.xml"
---
+++ modmain.lua
@@ -0,0 +1,10 @@
+local function pigmanpoop(inst)
+    inst:AddComponent("periodicspawner")
+    inst.components.periodicspawner:SetPrefab("poop")
+    inst.components.periodicspawner:SetRandomTimes(40, 60)
+    inst.components.periodicspawner:SetDensityInRange(20, 2)
+    inst.components.periodicspawner:SetMinimumSpacing(8)
+    inst.components.periodicspawner:Start()
+end
+
+AddPrefabPostInit("pigman", pigmanpoop)
